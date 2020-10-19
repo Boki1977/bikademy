1f76deacbe158f722318df31c00eebc3ef1a0f7d
Boki1977 <bpredmer@gmail.com>
2020-10-19 18:24:23 +0200

    Upload

--- include/page/exam/add_photo.php
+++ include/page/exam/add_photo.php
@@ -1,4 +1,17 @@
-
+ <?php
+	      
+		$result = $con->query("
+		SELECT * FROM lokacija
+		WHERE id='$lokacija_id' 
+		");
+							
+		while ($obj=mysqli_fetch_object($result))
+		{  
+		
+		include ("include/libs/lokacija.php");
+		}
+		
+		?>
         
 				<div style="margin-top:35%;"></div>
 	
@@ -8,9 +21,12 @@
                 <div class="card card-style">
 				
 				<input type="file" class="upload-file" accept="image/*" name="image" id="file"  onchange="loadFile(event)">
-                 
+                 <div id="confirm" style="display: none; text-align:center;"><h4>Confirm exam location</h4></div>
                    
-              <p><img id="output" width="100%" /></p>
+                 <p><img id="output" width="80%" style="margin-left:10%;"/>
+			  
+			  <div id="confirm2" style="display: none; text-align:center;"><h5>Click on photo to confirm</h5></div> 
+			  <div id="confirm3" style="display: none; text-align:center;"><h3><?php echo $naziv; ?></h3></div> 
 
 <script>
 var loadFile = function(event) {
@@ -18,6 +34,9 @@ var loadFile = function(event) {
 	image.src = URL.createObjectURL(event.target.files[0]);
 	
 	$("#file").hide();
+	$("#confirm").show();
+	$("#confirm2").show();
+	$("#confirm3").show();
 };
 </script>
                             
