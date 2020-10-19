e155f7300548a38bc56fb76a7fe40087112f9f21
Boki1977 <bpredmer@gmail.com>
2020-10-19 16:50:10 +0200

    Upload

--- include/page/exam/add_photo.php
+++ include/page/exam/add_photo.php
@@ -1,17 +1,18 @@
 
         
-				<div style="margin-top:45%;"></div>
+				<div style="margin-top:35%;"></div>
 	
 	
 					
 				
-                <div class="card card-style" style="background-color:<?php echo $card_color; ?>;">
-                    <img src="<?php echo $thumb; ?>" class="img-fluid">
-                    <div class="content pb-1">
-                     <h5 style="color:white;"><?php echo $naziv; ?></h5>
-                    </div>          
+                <div class="card card-style">
+                    <img src="data/source/icons/upload.png" class="img-fluid">
+                            
                 </div>      
 			
             
-       
+				
+                    <img src="data/source/icons/photo_hand.png" class="img-fluid" style="height:80%; margin-left:20%;">
+                            
+                
         
